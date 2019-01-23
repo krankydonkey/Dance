3386f50f2208e39f58e631fa9a86514021772331
thekrankydonkey <dvankran@gmail.com>
2019-01-23 21:09:15 +1000

    button working without transition

--- populate.js
+++ populate.js
@@ -1,28 +1,35 @@
-function make_event(event_name, level, start_time, end_time, discription) {
+function make_event(event) {
+    /* 
+    <button class="collapsible">
+        <p>event_name - level</p>
+        <p><em>start_time - end_time</em></p>
+    </button>
+    <div class="event">
+        <p>discription</p>
+    <\div>
+    */
+    var events = document.getElementById("events");
     var button = document.createElement("button");
     button.setAttribute("class", "collapsible");
     var nameField = document.createElement("p");
-    var title = event_name + " - " + level;
-    var names = document.createTextNode(title);
+    var names = document.createTextNode(event.name + " - " + event.ability);
     nameField.appendChild(names);
     var timeField = document.createElement("p");
     var timeItal = document.createElement("em");
-    timeRange = start_time + " - " + end_time;
-    var time = document.createTextNode(timeRange);
+    var time = document.createTextNode(event.start + " - " + event.end);
     timeItal.appendChild(time);
     timeField.appendChild(timeItal);
-
     button.appendChild(nameField);
     button.appendChild(timeField);
-
     var info = document.createElement("div");
     info.setAttribute("class", "event");
     var textField = document.createElement("p");
-    var text = document.createTextNode(discription);
+    var text = document.createTextNode(event.description);
     textField.appendChild(text);
     info.appendChild(textField);
     events.appendChild(button);
     events.appendChild(info);
+
 
     button.addEventListener("click", function() {
         if (info.style.maxHeight){
@@ -33,9 +40,26 @@ function make_event(event_name, level, start_time, end_time, discription) {
     });
 }
 
-make_event("Zouk Social", "All", "7:00pm", "7:30pm", "Come join us for a fun \
-        evening of social dancing! Open to those of all ages and abilities.");
+const events = [    
+    {
+        "name": "Zouk Social",
+        "ability": "All",
+        "start": "7:00pm",
+        "end": "7:30pm",
+        "company": "Cloud 9 Zouk",
+        "description": "Come join us for a fun evening of social dancing! Open to those of all ages and abilities."
+    },
+    {
+        "name": "Salsa Class",
+        "ability": "Intermediate",
+        "start": "6:00pm",
+        "end": "7:00pm",
+        "company": "50/50",
+        "description": "Come try our exciting block of classes."
+    }
+];
 
-make_event("Salsa Class", "Intermediate", "6:00pm", "7:00pm", "Come try our \
-        exciting block of classes.");
+for (var i = 0; i < events.length; i++) {
+    make_event(events[i]);
+}
     
