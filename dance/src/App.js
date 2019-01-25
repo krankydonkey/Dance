3354c7ea76907b190fec2d959811f56bd0855961
thekrankydonkey <dvankran@gmail.com>
2019-01-25 13:11:45 +1000

    help

--- dance/src/App.js
+++ dance/src/App.js
@@ -24,52 +24,13 @@ class App extends Component {
     
     return (
       <div id="events">
-        {events.map(event => <Event{...event}/>)}
+        {events.map(event => <Collapsible{...event}/>)}
       </div>
     );
   }
 }
 
 
-class Event extends Component {
-  constructor(props) {
-    super(props);
-
-    // Initial state
-    this.state = {
-      open: false,
-      maxHeight: 0
-    };
-    this.toggle = this.toggle.bind(this);
-  }
-  // Toggles the status of open
-  toggle(description) {
-    this.setState({open: !this.state.open});
-    if (this.state.open) {
-      description.style.maxHeight = description.scrollHeight + "px";
-    } else {
-      description.style.maxHeight = 0;
-    }
-  }
-
-  render() {
-    var description = 
-      <div className="collapsible">
-        {this.props.description}
-      </div>;
-    return (
-      <div className="event">
-        <button className="event-btn" onClick={this.toggle(description)}>
-          <p>{this.props.name} - {this.props.ability}</p>
-          <p><em>{this.props.start} - {this.props.end}</em></p>
-        </button>
-        {description}
-      </div>
-    );
-  }
-}
- 
-// Ignore This
 class Collapsible extends Component {
   constructor(props) {
     super(props);
@@ -79,17 +40,18 @@ class Collapsible extends Component {
 
   toggle() {
     this.setState({open: !this.state.open});
-    if (this.state.open) {
-      this.style.maxHeight = this.scrollHeight + "px";
-    } else {
-      this.style.maxHeight = 0;
-    }
   }
 
-  render() {      
+  render() {
     return (
-      <div className="collapsible">
-        {this.props.description}
+      <div className="event">
+        <button className="event-btn" onClick={this.toggle}>
+          <p>{this.props.name} - {this.props.ability}</p>
+          <p><em>{this.props.start} - {this.props.end}</em></p>
+        </button>
+        <div className="collapsible" style={this.state.open ? {maxHeight: 350} : {maxHeight: 0}}>
+          {this.props.description}
+        </div>;
       </div>
     );
   }
